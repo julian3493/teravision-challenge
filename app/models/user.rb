ff89a3cd4089f5f0e86f478b7f3295c2e603451d
julian3493 <julianramosarevalo@gmail.com>
2021-05-06 12:29:34 -0500

    add validations in user model and add bulma.io

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :fullname, presence: true, length: { in: 6..60 }
+  validates :birthdate, presence: true
+  validates :gender, presence: true
+  validates :imageURL, presence: true
 end
